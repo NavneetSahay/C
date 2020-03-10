0558c6608479c948f7a1a3460c4934d75605b248
NAVNEET SAHAY <56154963+NavneetSahay@users.noreply.github.com>
2020-03-10 07:18:33 +0530

    Add files via upload

---
+++ even_odd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    printf("Enter a number\n");
+    scanf("%d",&num);
+    if(num%2==0)
+    printf("Num is even\n");
+    else
+    {
+        printf("num is odd\n");
+    }
+    
+}
